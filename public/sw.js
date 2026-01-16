5c1e72b3fe8f5c9f12977f8e77881e779d007c8d
Guilherme Moura <glhrmoura@gmail.com>
2026-01-16 11:43:28 -0300

    chore: bump service worker version to 8

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = 7;
+const APP_VERSION = 8;
 const CACHE_NAME = `cash-app-v${APP_VERSION}-${Date.now()}`;
 const STATIC_CACHE = `cash-app-static-v${APP_VERSION}`;
 const DYNAMIC_CACHE = `cash-app-dynamic-v${APP_VERSION}`;
