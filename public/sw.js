011a6a29613ea3f42df97d369f1f31d9d7f7e100
Guilherme Moura <glhrmoura@gmail.com>
2026-07-31 10:18:21 -0300

    chore: bump version to 0.1.18

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = '0.1.17';
+const APP_VERSION = '0.1.18';
 const CACHE_NAME = `cash-app-v${APP_VERSION}`;
 const STATIC_CACHE = `cash-app-static-v${APP_VERSION}`;
 const DYNAMIC_CACHE = `cash-app-dynamic-v${APP_VERSION}`;
